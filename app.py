2a3f5a5bc17901cb1eaa35e51d91c3a9841f40ce
Karol Radecki <radeckikarol7@gmail.com>
2025-04-20 17:56:27 +0200

    2

--- app.py
+++ app.py
@@ -17,4 +17,5 @@ def decyzja_api():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5050)
+
